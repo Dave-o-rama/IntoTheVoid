0229316797d2383974dc22e5abd384b70fa3aa6c
Dave <davedontalk@gmail.com>
2016-11-19 09:30:01 -0500

    Script for cubes adjacent to iron cubes, will probably end up scrapping it

---
+++ Assets/Scripts/IronAdjacentCubes.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IronAdjacentCubes : MonoBehaviour {
+	
+	GameObject[] thingsToDestroy;
+	public GameObject regularCube;
+	public int cubesToDestroy = 0;
+	public bool willCheckOtherCubes;
+
+	void Start(){
+		thingsToDestroy = new GameObject[24];
+
+		int j = Random.Range (0, 100);
+		if (j <= 20) {
+			willCheckOtherCubes = true;
+		} else {
+			willCheckOtherCubes = false;
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "cannotClickCube") {
+			thingsToDestroy [cubesToDestroy] = col.gameObject;
+			cubesToDestroy++;
+		}
+
+		if (cubesToDestroy <= 6 && willCheckOtherCubes) {
+			int j = Random.Range (0, thingsToDestroy.Length);
+			Instantiate (regularCube, thingsToDestroy[j].transform.position, Quaternion.identity);
+			Destroy (thingsToDestroy [j]);
+		}
+	}
+
+	
+}
