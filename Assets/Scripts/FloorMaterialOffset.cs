da6e2f44b41b16976d0435c2ff1413cefcb86f9e
Dave <davedontalk@gmail.com>
2016-11-30 14:36:10 -0500

    Rainbow road officially great again

---
+++ Assets/Scripts/FloorMaterialOffset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorMaterialOffset : MonoBehaviour {
+
+	public float scrollSpeed = 0.5F;
+	private MeshRenderer rend;
+
+	void Start() {
+		rend = GetComponent<MeshRenderer>();
+	}
+
+	void Update() {
+		float offset = Time.time * scrollSpeed;
+		rend.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+	}
+}
